9c7d91954f55f86551c7e1b936df494f99a169a9
Speed <rc0029514@student.riversidecollege.ac.uk>
2019-03-04 15:44:54 +0000

    Add project files.

---
+++ areas/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace areas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double length = double.Parse(textBox1.Text);
+            double width = double.Parse(textBox2.Text);
+            double area = length * width;
+            textBox3.AppendText(area.ToString());
+
+        }
+    }
+}
